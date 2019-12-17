7c378f0f348b863c801ba1388d2c716913eed3de
Dim04ka <suhotsky.dmitry@gmail.com>
2019-12-18 00:15:25 +0300

    todo list

---
+++ precache-manifest.e7f742d5077d29731362d3976a476ba7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd41089d9fdb73ecce95da54d1810040",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24c51aafbef28e0f9c63",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "d5686f21f0002ba67a15",
+    "url": "/static/css/main.074d803a.chunk.css"
+  },
+  {
+    "revision": "24c51aafbef28e0f9c63",
+    "url": "/static/js/2.205ab072.chunk.js"
+  },
+  {
+    "revision": "262564d489e1513f5d315bbf4bcd7b14",
+    "url": "/static/js/2.205ab072.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d5686f21f0002ba67a15",
+    "url": "/static/js/main.76621c29.chunk.js"
+  },
+  {
+    "revision": "35fec85e1988fda5a58a",
+    "url": "/static/js/runtime-main.3c491052.js"
+  }
+]);
